9c7123131d71777f3d9586d5756794fd8a656e51
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-05-02 19:02:30 -0300

    populando itens da api

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from "react-redux";
+import api from '../services/api';
+import { IProduct } from '../store/modules/cart/types';
 
 export default function Catalog() {
 
-	const catalog = useSelector(state => state)
-	console.log(catalog);
+	const [catalog, setCatalog] = useState<IProduct[]>([])
+
+	useEffect(() => {
+		api.get('products').then(response => {
+			setCatalog(response.data)
+		})
+	})
+
+	// const catalog = useSelector(state => state)
+	// console.log(catalog);
 
 	return (
-		<h1>Catalog</h1>
+		<>
+			<h1>Catalog</h1>
+			{catalog.map(product => (
+				<article key={product.id}>
+					<strong>{product.title}</strong> {" - "}
+					<span>{product.price}</span> {"  "}
+
+					<button type="button">
+						Comprar
+					</button>
+				</article>
+			))}
+		</>
 	)
 }
