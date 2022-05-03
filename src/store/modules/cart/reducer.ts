4ce96c0bb19f9d7aa4b1e3947834736a7a3ec2c2
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-05-03 14:00:29 -0300

    imutabilidade com immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from "immer";
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -6,24 +7,23 @@ const INITIAL_STATE: ICartState = {
 }
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'ADD_PRODUCT_TO_CART': {
-            const { product } = action.payload;
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {
-                        product,
-                        quantity: 1
-                    }
-                ]
-            };
+    return produce(state, draft => {
+        switch (action.type) {
+            case 'ADD_PRODUCT_TO_CART': {
+                const { product } = action.payload;
+    
+                draft.items.push({
+                    product,
+                    quantity: 1
+                })
+
+                break;
+            }
+            default: {
+                return draft;
+            }
         }
-        default: {
-            return state;
-        }
-    }
+    })
 }
 
 export default cart;
