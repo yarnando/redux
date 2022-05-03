552ad2747f0c001198461790358e5648ffb459df
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-05-03 14:51:52 -0300

    catalog item abstraido

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from "react-redux";
 import api from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import CatalogItem from './CatalogItem';
 
 export default function Catalog() {
-
-	const dispatch = useDispatch();
 	const [catalog, setCatalog] = useState<IProduct[]>([])
 
 	useEffect(() => {
@@ -15,27 +12,14 @@ export default function Catalog() {
 		})
 	}, [])
 
-	const handleAddProductToCart = useCallback( (product: IProduct) => {
-		dispatch(addProductToCart(product))
-	}, [dispatch])
-
-	// const catalog = useSelector(state => state)
-	// console.log(catalog);
-
 	return (
 		<>
 			<h1>Catalog</h1>
 			{catalog.map(product => (
-				<article key={product.id}>
-					<strong>{product.title}</strong> {" - "}
-					<span>{product.price}</span> {"  "}
-
-					<button 
-						type="button"
-						onClick={() => handleAddProductToCart(product)}>
-						Comprar
-					</button>
-				</article>
+				<CatalogItem 
+					key={product.id}
+					product={product}
+				/>
 			))}
 		</>
 	)
