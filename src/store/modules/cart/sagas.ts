ab2620181a8c0d9527f7b20155be7815bdf77257
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-05-03 19:33:48 -0300

    checando quantidade no saga

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,12 +1,23 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, select, takeLatest } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductToCart } from "./actions";
+// takeLatest => cancela a ação feita anteriormente e faz uma nova (evita que o usuario clique e chame a api varias vezes)
+// takeEvery => dispara a ação sempre e espera todas
+// takeLeading => pega só a primeira ação, ignora as proximas ate essa primeira acabar
 
-function checkProductStock() {
-    console.log('Adicionou ao carrinho');
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+    const { product } = payload;
+
+    const currentQuantity: number = yield select( (state: IState) => {
+        return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+    })
+
+    console.log(currentQuantity);
+    
 }
 
 export default all([
     takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
-    // takeLatest => cancela a ação feita anteriormente e faz uma nova (evita que o usuario clique e chame a api varias vezes)
-    // takeEvery => dispara a ação sempre e espera todas
-    // takeLeading => pega só a primeira ação, ignora as proximas ate essa primeira acabar
 ])
