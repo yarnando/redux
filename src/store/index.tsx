6d65b21128dfd95e7a1688adf0bc0ebd34cb9871
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-05-03 14:47:08 -0300

    redux devtools

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,4 +1,6 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import rootReducer from "./modules/rootReducer";
 
 import { ICartState } from "./modules/cart/types";
@@ -6,7 +8,10 @@ export interface IState {
     cart: ICartState;
 }
 
-const store = createStore(rootReducer)
+const store = createStore(
+    rootReducer,
+    composeWithDevTools()
+)
 
 export default store;
 
